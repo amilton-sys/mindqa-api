5a011d5046f35caf33920a4110c48e8cdd2ecc89
Amilton <amiltonjose.pe@gmail.com>
2024-08-14 21:00:36 -0300

    feat: criado projeto inicial

---
+++ src/main/java/com/sys/mindqa/MindQaApplication.java
@@ -0,0 +1,13 @@
+package com.sys.mindqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindQaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindQaApplication.class, args);
+    }
+
+}
