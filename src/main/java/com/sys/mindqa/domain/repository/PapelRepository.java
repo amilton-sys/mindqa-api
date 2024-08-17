4236f418df025935761629a5a9ad009d6f6c5707
Amilton <amiltonjose.pe@gmail.com>
2024-08-16 22:39:25 -0300

    feat: adiciona o PapelRepository

---
+++ src/main/java/com/sys/mindqa/domain/repository/PapelRepository.java
@@ -0,0 +1,7 @@
+package com.sys.mindqa.domain.repository;
+
+import com.sys.mindqa.domain.model.Papel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PapelRepository extends JpaRepository<Papel, Integer> {
+}
